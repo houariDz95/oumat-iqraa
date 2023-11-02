56f27de3493b99a05bd3df0d500da070a4561a34
eddDevDz95 <houedd608@gmail.com>
2023-11-02 17:59:54 +0100

    new commit

--- app/direct/[keyword]/page.js
+++ app/direct/[keyword]/page.js
@@ -1,19 +1,21 @@
 "use client"
-import { useEffect } from "react"
 import { useRouter } from "next/navigation"
 import BannerSq from "@/banners/BannerSq"
 import Script from "next/script"
 
 const page = () => {
     const router = useRouter()
-    useEffect(() => {
-        router.push('https://www.toprevenuegate.com/gvybxabv?key=a56a58d60fd2d3fdac49b5f67583ada4')
-    }, [])
   return (
-    <div className="max-6xl mx-auto min-h-screen bg-zinc-500 flex items-center justify-center">
+    <div className="max-6xl mx-auto min-h-screen bg-zinc-500 flex items-center justify-center  flex-col">
         <BannerSq />
         <Script type='text/javascript' src='//pl20816003.toprevenuegate.com/bf/9e/b6/bf9eb6e7b5ddd3ce92701feb9b883409.js' />
         <Script async="async" data-cfasync="false" src="//pl20812775.toprevenuegate.com/6b3890282dbcd2ff77e5aedcafd49c1a/invoke.js" />
+        <button 
+        onClick={() => router.push('https://www.toprevenuegate.com/e767z6fz?key=f1dc2a62bf2183080fdc32f1321ce96e')}
+        className="bg-green-500 w-[200px] p-4 rounded-xl text-white font-semobold"
+        >
+          Download
+          </button>
         <div id="container-6b3890282dbcd2ff77e5aedcafd49c1a"></div>
     </div>
   )
