3b4e2738ec4b61f77bb349b7282f55fc10f82113
eddDevDz95 <houedd608@gmail.com>
2023-11-02 22:39:45 +0100

    update direct page

--- app/direct/[keyword]/page.js
+++ app/direct/[keyword]/page.js
@@ -1,6 +1,6 @@
 "use client"
-import { useRouter } from "next/navigation"
 import BannerSq from "@/banners/BannerSq"
+import Link from "next/link"
 import Script from "next/script"
 
 const page = () => {
@@ -10,12 +10,13 @@ const page = () => {
         <BannerSq />
         <Script type='text/javascript' src='//pl20816003.toprevenuegate.com/bf/9e/b6/bf9eb6e7b5ddd3ce92701feb9b883409.js' />
         <Script async="async" data-cfasync="false" src="//pl20812775.toprevenuegate.com/6b3890282dbcd2ff77e5aedcafd49c1a/invoke.js" />
-        <button 
-        onClick={() => router.push('https://www.toprevenuegate.com/e767z6fz?key=f1dc2a62bf2183080fdc32f1321ce96e')}
+        <Link 
+        href="https://www.toprevenuegate.com/e767z6fz?key=f1dc2a62bf2183080fdc32f1321ce96e"
         className="bg-green-500 w-[200px] p-4 rounded-xl text-white font-semobold"
+        target="_blank"
         >
           Download
-          </button>
+          </Link>
         <div id="container-6b3890282dbcd2ff77e5aedcafd49c1a"></div>
     </div>
   )
