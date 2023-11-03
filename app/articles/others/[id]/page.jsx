45d6dfbc31912cb43b2fff6c40637bc69682158a
eddDevDz95 <houedd608@gmail.com>
2023-11-03 19:44:35 +0100

    new commit

--- app/articles/others/[id]/page.jsx
+++ app/articles/others/[id]/page.jsx
@@ -4,6 +4,7 @@ import Sidebar from '@/components/Sidebar'
 import ArticleDetailsOther from '@/components/articles/ArticleDetailsOther';
 import { getPost, readMore } from '@/actions';
 import { atCategories } from '@/constants';
+import Script from 'next/script';
 
 export async function generateMetadata({params: {id}}){
 
@@ -57,6 +58,12 @@ const page = async ({params: {id}}) => {
           <Sidebar categories={atCategories} articles/>
         </div>
       </div>
+      <div className='max-w-6xl mx-auto flex items-center justify-center'>
+        <Script async="async" data-cfasync="false" src="//pl21201781.toprevenuegate.com/6638e496d98320ae6eda5fbad9755f56/invoke.js" />
+        <div id="container-6638e496d98320ae6eda5fbad9755f56"></div>
+      </div>
+      <Script type='text/javascript' src='//pl21201816.toprevenuegate.com/9a/09/61/9a0961c3a8eb0e15c0cd5bb0ed6b5c2e.js' />
+      <Script type='text/javascript' src='//pl21201883.toprevenuegate.com/cb/a7/24/cba724b168d4299365eee7b175dfab00.js' />
     </>
   )
 }
--- components/articles/ArticleDetailsOther.jsx
+++ components/articles/ArticleDetailsOther.jsx
@@ -5,8 +5,8 @@ import 'moment/locale/ar';
 import Image from "next/image";
 import { updateText } from "@/utils/updateText";
 import dynamic from "next/dynamic";
-import BannerTl from "@/banners/BannerTl";
-import BannerLong from "@/banners/BannerLong";
+import BannerSq from "@/banners/BannerSq";
+import BannerSm from "@/banners/BannerSm";
 
 const ReadMore = dynamic(() => import("./ReadMore"), {ssr: false});
 
@@ -16,6 +16,9 @@ const ArticleDetailsOther = ({imageUrl, title, date, articleText, isFromEditor, 
     <div
       className="max-w-2xl flex-1 p-4 md:p-0 mx-auto"
     >
+      <div className="max-w-6xl mx-auto flex items-center my-8 justify-center">
+        <BannerSm />
+      </div>
       <div className="flex items-center gap-2 mb-4">
         <p className="text-gray-500 flex items-center gap-2 text-sm">
         <AiOutlineCalendar size={18} color='#6449ff' />
@@ -39,7 +42,7 @@ const ArticleDetailsOther = ({imageUrl, title, date, articleText, isFromEditor, 
           <span>*</span>
       </div>
       <div className="max-w-6xl mx-auto flex items-center my-8 justify-center">
-        <BannerLong />
+        <BannerSq />
       </div>
       <ReadMore posts={readMore} />
     </div>
