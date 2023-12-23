434c2e126e987b6d37e159b2ceb28bc7e094f875
eddDevDz95 <houedd608@gmail.com>
2023-12-23 17:52:17 +0100

    update jsx

--- Banners/Banner_350.js
+++ Banners/Banner_350.js
@@ -9,7 +9,7 @@ const Banner_350 = () => {
         target="_blank">
             <img 
             src="//ylx-aff.advertica-cdn.com/pub/300x250.png" 
-            style="border:none;margin:0;padding:0;vertical-align:baseline;" 
+            style={{border:" none", margin:0, padding:0, verticalAlign: "baseline"}} 
             alt="ylliX - Online Advertising Network" />
         </a>
       </div>
