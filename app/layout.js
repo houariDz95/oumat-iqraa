b9884043e3329d703f4a398c76303d7f08f521ad
eddDevDz95 <houedd608@gmail.com>
2024-02-10 11:36:59 +0100

    add hilltopsads

--- app/layout.js
+++ app/layout.js
@@ -19,6 +19,7 @@ export const metadata = {
     "og:url": "oumat-iqraa.com",
     "og:image": '/assets/library.png',
     "og:type": "website",
+    "0821cf729c181b00a13a8f77587bd37ad3e22adf" : "0821cf729c181b00a13a8f77587bd37ad3e22adf"
   }
 };
 
