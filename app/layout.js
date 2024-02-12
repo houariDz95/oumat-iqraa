fbd2d48fe8ebd969833871e08815737b93a9e3df
eddDevDz95 <houedd608@gmail.com>
2024-02-12 12:07:03 +0100

    new commit

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,7 @@ export const metadata = {
     "og:url": "oumat-iqraa.com",
     "og:image": '/assets/library.png',
     "og:type": "website",
-    "0821cf729c181b00a13a8f77587bd37ad3e22adf" : "0821cf729c181b00a13a8f77587bd37ad3e22adf"
+   // "0821cf729c181b00a13a8f77587bd37ad3e22adf" : "0821cf729c181b00a13a8f77587bd37ad3e22adf"
   }
 };
 
@@ -63,7 +63,7 @@ export default function RootLayout({ children }) {
             `,
           }}
         />
-      {/*<Script
+      <Script
       id='thunder'
       type="text/javascript"
         dangerouslySetInnerHTML={{
@@ -81,33 +81,7 @@ export default function RootLayout({ children }) {
             };
           `,
         }}
-      />*/}
-         <Script 
-         id="in push"
-         dangerouslySetInnerHTML={{ __html: `
-          (function(ntf){
-            var d = document,
-                s = d.createElement('script'),
-                l = d.scripts[d.scripts.length - 1];
-            s.settings = ntf || {};
-            s.src = "//crookedchange.com/auWU5Cw.Y/W/dLlxQe2/9fkDZTTf9U6Kb/2_5YlnSBWGQY9ONgTMEw5sM/z-Y/0/MQiK0H1xMeT/kFziNhjdQwzy";
-            s.async = true;
-            l.parentNode.insertBefore(s, l);
-          })({});
-         ` }} />
-          <Script
-          id="in push page" 
-          dangerouslySetInnerHTML={{ __html: `
-            (function(ria){
-              var d = document,
-                  s = d.createElement('script'),
-                  l = d.scripts[d.scripts.length - 1];
-              s.settings = ria || {};
-              s.src = "//crookedchange.com/a.Ws5hw/YQWsd-lzQJ2/9OkLZ/TU9/6ZbV2a5jlkS/WzQJ9BNrToEx5jMdz/YP1FM/ii0u1EMXT/kDz/NvjtUFzq";
-              s.async = true;
-              l.parentNode.insertBefore(s, l);
-            })({});
-          `}} />
+      />
         <Script async="async" data-cfasync="false" src="//pl22011376.toprevenuegate.com/8d0e5ef90b16b8673778be8c227df469/invoke.js" />
         <Script type='text/javascript' src='//pl22011379.toprevenuegate.com/cb/6c/a9/cb6ca9bc4f527eb49d7f74b6747bcd7a.js' />
       </body>
