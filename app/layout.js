6ee112d2d1b6ab78fd8c4ce65ca3acd8d6dfae3b
eddDevDz95 <houedd608@gmail.com>
2024-02-15 00:22:43 +0100

    new commit

--- app/layout.js
+++ app/layout.js
@@ -20,7 +20,7 @@ export const metadata = {
     "og:image": '/assets/library.png',
     "og:type": "website",
    // "0821cf729c181b00a13a8f77587bd37ad3e22adf" : "0821cf729c181b00a13a8f77587bd37ad3e22adf"
-   "monetag":"db9ec2b75c10e144f6002c44db63a127"
+   "monetag": "db9ec2b75c10e144f6002c44db63a127"
   }
 };
 
@@ -46,7 +46,7 @@ export default function RootLayout({ children }) {
               }}
           />
         </div>
-        <Script
+        {/*<Script
           id='popcach_ads'
           type="text/javascript"
           dangerouslySetInnerHTML={{
@@ -82,7 +82,7 @@ export default function RootLayout({ children }) {
             };
           `,
         }}
-      />
+      />*/}
         <Script async="async" data-cfasync="false" src="//pl22011376.toprevenuegate.com/8d0e5ef90b16b8673778be8c227df469/invoke.js" />
         <Script type='text/javascript' src='//pl22011379.toprevenuegate.com/cb/6c/a9/cb6ca9bc4f527eb49d7f74b6747bcd7a.js' />
       </body>
