d5ac1f6d96405edae8919acd7e0ae3b2e3f2e6f6
eddDevDz95 <houedd608@gmail.com>
2024-02-18 16:10:15 +0100

    new commit

--- app/layout.js
+++ app/layout.js
@@ -19,45 +19,14 @@ export const metadata = {
     "og:url": "oumat-iqraa.com",
     "og:image": '/assets/library.png',
     "og:type": "website",
-    "0821cf729c181b00a13a8f77587bd37ad3e22adf" : "0821cf729c181b00a13a8f77587bd37ad3e22adf"
+    "monetag": "db9ec2b75c10e144f6002c44db63a127"
   }
-};
+}; 
+
+//"0821cf729c181b00a13a8f77587bd37ad3e22adf" : "0821cf729c181b00a13a8f77587bd37ad3e22adf"
 
 
 export default function RootLayout({ children }) {
-  const scriptContent = `
-    (function(buorz){
-      var d = document,
-          s = d.createElement('script'),
-          l = d.scripts[d.scripts.length - 1];
-      s.settings = buorz || {};
-      s.src = "//crookedchange.com/aEWR5TwyY.Wxd/lWQK2U9pkKZPT/9R6Kbw2v5clcSvWDQu9GNbTdEz5TMUz-YK0YMuie0k1IMaTUkOzpNUjBQfzF";
-      s.async = true;
-      l.parentNode.insertBefore(s, l);
-    })({});
-  `;
-  const scriptContentTwo = `
-  (function(iappn){
-    var d = document,
-        s = d.createElement('script'),
-        l = d.scripts[d.scripts.length - 1];
-    s.settings = iappn || {};
-    s.src = "//crookedchange.com/a.W/5owGYuWddsllQ/2R9/k/ZITc9B6_bM2/5blFSwWqQO9/N/TGEc5GMAzCYq1/MSiK0d1UMYTbkezlNwjDU/zr";
-    s.async = true;
-    l.parentNode.insertBefore(s, l);
-  })({});
-`;
-const scriptContentThree = `
-    (function(bc){
-      var d = document,
-          s = d.createElement('script'),
-          l = d.scripts[d.scripts.length - 1];
-      s.settings = bc || {};
-      s.src = "//crookedchange.com/bYXrVHs/d.GDl/0_YmWCdbiSYPWn5Du/ZZXDIn/be/my9TuUZ/UCl/kbPlTeUyx/OfTsM-2ZM_jgICtkNNTPEF5HMtz/YwyYM/wj";
-      s.async = true;
-      l.parentNode.insertBefore(s, l);
-    })({});
-  `;
   return (
     <html lang="ar">
       <body className={kufi.className}>
@@ -78,7 +47,7 @@ const scriptContentThree = `
               }}
           />
         </div>
-        {/* <Script
+        <Script
           id='popcach_ads'
           type="text/javascript"
           dangerouslySetInnerHTML={{
@@ -95,29 +64,26 @@ const scriptContentThree = `
               };
             `,
           }}
-        /> */}
-      {/* <Script id="in zone" dangerouslySetInnerHTML={{ __html: scriptContent }} /> */}
-      <Script id="in push" dangerouslySetInnerHTML={{ __html: scriptContentTwo }} />
-      <Script id="multi" dangerouslySetInnerHTML={{ __html: scriptContentThree }} />
-      {/* <Script
-      id='thunder'
-      type="text/javascript"
-        dangerouslySetInnerHTML={{
-          __html: `
-            var uid = '467853';
-            var wid = '707751';
-            var pop_fback = 'up';
-            var pop_tag = document.createElement('script');
-            pop_tag.src = '//cdn.popcash.net/show.js';
-            document.body.appendChild(pop_tag);
-            pop_tag.onerror = function () {
-              pop_tag = document.createElement('script');
-              pop_tag.src = '//cdn2.popcash.net/show.js';
+        /> 
+        <Script
+        id='thunder'
+        type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var uid = '467853';
+              var wid = '707751';
+              var pop_fback = 'up';
+              var pop_tag = document.createElement('script');
+              pop_tag.src = '//cdn.popcash.net/show.js';
               document.body.appendChild(pop_tag);
-            };
-          `,
-        }}
-      /> */}
+              pop_tag.onerror = function () {
+                pop_tag = document.createElement('script');
+                pop_tag.src = '//cdn2.popcash.net/show.js';
+                document.body.appendChild(pop_tag);
+              };
+            `,
+          }}
+        />
         <Script async="async" data-cfasync="false" src="//pl22011376.toprevenuegate.com/8d0e5ef90b16b8673778be8c227df469/invoke.js" />
         <Script type='text/javascript' src='//pl22011379.toprevenuegate.com/cb/6c/a9/cb6ca9bc4f527eb49d7f74b6747bcd7a.js' />
       </body>
