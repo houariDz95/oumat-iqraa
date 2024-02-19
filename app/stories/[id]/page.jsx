8a4cfc1bc6a11a851bf522502790c65ca53b3ad3
eddDevDz95 <houedd608@gmail.com>
2024-02-19 22:47:48 +0100

    new commit

--- app/stories/[id]/page.jsx
+++ app/stories/[id]/page.jsx
@@ -44,7 +44,7 @@ const Story = async ({params: {id}}) => {
       <Navbar />
       <PageTitle title={post.title} desc="استمتع بقراءة قصة شيقة ومثيرة تغطي موضوعًا مهمًا" />
       <div className="max-w-[720px] mx-auto my-2">
-        <Banner_720 />
+        <Banner_480 />
       </div>
       <div className="max-w-6xl mx-auto  min-h-[calc(100vh-73px)] flex mt-10">
         <div className="flex-1 lg:flex-[0.75]">
