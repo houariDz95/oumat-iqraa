c9f490740dd6d47d553322eeadaf359fe692a2a8
eddDevDz95 <houedd608@gmail.com>
2024-03-17 15:56:57 +0100

    new commit

--- components/articles/ArticleDetailsOther.jsx
+++ components/articles/ArticleDetailsOther.jsx
@@ -1,6 +1,5 @@
 'use client';
 
-import Banner_480 from "@/Banners/Banner_480";
 import Banner_720 from "@/Banners/Banner_720";
 import { updateText } from "@/utils/updateText";
 
@@ -11,7 +10,7 @@ const ArticleDetailsOther = ({articleText, isFromEditor}) => {
     <div
       className="col-span-12  lg:col-span-8 font-in prose sm:prose-base md:prose-lg max-w-max"
     >
-      <Banner_480 />
+      <Banner_720 />
       <div className="mb-5 col-span-12  lg:col-span-8 font-in prose sm:prose-base md:prose-lg max-w-max
           prose-blockquote:bg-accent/20 
           prose-blockquote:p-2
