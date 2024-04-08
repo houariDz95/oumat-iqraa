0bbef0a6993be5b7f2ec625766ad61eff319a909
eddDevDz95 <houedd608@gmail.com>
2024-04-08 15:38:33 +0100

    new commit

--- app/(content)/articles/page.jsx
+++ app/(content)/articles/page.jsx
@@ -1,36 +1,26 @@
-// import Main from "@/components/articles/Main";
-// import { atCategories } from "@/constants";
-// import { getArticles } from "@/actions";
+import Main from "@/components/articles/Main";
+import { atCategories } from "@/constants";
+import { getArticles } from "@/actions";
  
-// export async function generateMetadata(paramKey){
-//   const cat = paramKey.searchParams.cat;
-//   const title = atCategories.filter(category => category.path === cat)[0].name;
-//   return {
-//     title: ` ${cat ? title : " كل المقالات"} - أمة اقرأ`,
-//     description: 'استمتع بقراءة واستكشاف مجموعة المقالات الشيقة في موقع أمة اقرأ.',
-//   }
-// }
-
-// const Articles = async (paramKey) => {
-//     const cat = paramKey.searchParams.cat;
-//     const allArticles = await getArticles(cat);
-    
-//   return (
-//     <>
-//       <Main allArticles={allArticles}/>
-//     </>
-//   )
-// }
-
-// export default Articles
+export async function generateMetadata(paramKey){
+  const cat = paramKey.searchParams.cat;
+  const title = atCategories.filter(category => category.path === cat)[0].name;
+  return {
+    title: ` ${cat ? title : " كل المقالات"} - أمة اقرأ`,
+    description: 'استمتع بقراءة واستكشاف مجموعة المقالات الشيقة في موقع أمة اقرأ.',
+  }
+}
 
 const Articles = async (paramKey) => {
-
-  
-return (
-  <>
-  </>
-)
+    const cat = paramKey.searchParams.cat;
+    const allArticles = await getArticles(cat);
+    
+  return (
+    <>
+      <Main allArticles={allArticles}/>
+    </>
+  )
 }
 
 export default Articles
+
