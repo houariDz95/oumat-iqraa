eb4f8084098f77653dc3ce6dd8ee476518ebd840
eddDevDz95 <houedd608@gmail.com>
2024-04-29 22:58:35 +0100

    new commit

--- components/articles/ArticleDetailsOther.jsx
+++ components/articles/ArticleDetailsOther.jsx
@@ -17,7 +17,7 @@ const ArticleDetailsOther = ({articleText, isFromEditor}) => {
     <div
       className="col-span-12  lg:col-span-8 font-in prose sm:prose-base md:prose-lg max-w-max"
     >
-      <Banner_480 />
+      <Banner_350 />
       <div className="mb-5 col-span-12  lg:col-span-8 font-in prose sm:prose-base md:prose-lg max-w-max
           prose-blockquote:bg-accent/20 
           prose-blockquote:p-2
