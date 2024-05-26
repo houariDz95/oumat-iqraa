6e49e81e4be5b0f7edd9e10b4d25284e5b08014e
eddDevDz95 <houedd608@gmail.com>
2024-05-26 23:24:53 +0100

    new commit

--- app/layout.js
+++ app/layout.js
@@ -30,7 +30,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ar">
         <head>
-        <script async="async" data-cfasync="false" src="//thubanoa.com/1?z=7431064"></script> 
+        {/* <script async="async" data-cfasync="false" src="//thubanoa.com/1?z=7431064"></script>  */}
          <Script 
             id="vignet"
               dangerouslySetInnerHTML={{
@@ -69,21 +69,7 @@ export default function RootLayout({ children }) {
         />
         
         <Analytics />
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4378697867992790"
-     crossorigin="anonymous"></script>
-        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4378697867992790" crossorigin="anonymous" />
-        <Script async="async" data-cfasync="false" src="//pl22011376.toprevenuegate.com/8d0e5ef90b16b8673778be8c227df469/invoke.js" />
-        {/*<Script type='text/javascript' src='//pl22011379.toprevenuegate.com/cb/6c/a9/cb6ca9bc4f527eb49d7f74b6747bcd7a.js' />*/}
-        <Script type='text/javascript' src='//pl22011379.profitablegatecpm.com/cb/6c/a9/cb6ca9bc4f527eb49d7f74b6747bcd7a.js' /> 
-      </body>
-    </html>
-  )
-}
-
-
-{/*
-
- <Script
+        <Script
           id='popcach_ads'
           type="text/javascript"
           dangerouslySetInnerHTML={{
@@ -120,5 +106,15 @@ export default function RootLayout({ children }) {
             `,
           }}
         />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4378697867992790"
+     crossorigin="anonymous"></script>
+        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4378697867992790" crossorigin="anonymous" />
+        <Script async="async" data-cfasync="false" src="//pl22011376.toprevenuegate.com/8d0e5ef90b16b8673778be8c227df469/invoke.js" />
+        {/*<Script type='text/javascript' src='//pl22011379.toprevenuegate.com/cb/6c/a9/cb6ca9bc4f527eb49d7f74b6747bcd7a.js' />*/}
+        <Script type='text/javascript' src='//pl22011379.profitablegatecpm.com/cb/6c/a9/cb6ca9bc4f527eb49d7f74b6747bcd7a.js' /> 
+      </body>
+    </html>
+  )
+}
 
-*/}
+
