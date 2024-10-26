13e6154e7c0b5d7699415fef6147e70392e15223
eddDevDz95 <houedd608@gmail.com>
2024-10-26 16:26:18 +0100

    new commit

--- app/(content)/components/RenderMdx.jsx
+++ app/(content)/components/RenderMdx.jsx
@@ -1,8 +1,7 @@
 "use client"
-import React, { useEffect } from 'react'
+import React, {useState, useEffect } from 'react'
 import { useMDXComponent } from 'next-contentlayer/hooks'
 import Image from 'next/image'
-import { useState, useEffect } from 'react'
 
 const mdxComponents = {
     Image
