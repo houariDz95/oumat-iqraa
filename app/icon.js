d7cf7bac5eb7fe527f0a616ba047913da780adb2
eddDevDz95 <houedd608@gmail.com>
2024-12-09 13:47:48 +0100

    new commit

--- app/icon.js
+++ app/icon.js
@@ -1,19 +1,21 @@
-import { ImageResponse } from 'next/server';
+import { ImageResponse } from 'next/og'
  
+// Image metadata
 export const size = {
-  width: 30,
-  height: 30,
-};
-export const contentType = 'image/png';
-export const runtime = 'edge';
+  width: 32,
+  height: 32,
+}
+export const contentType = 'image/png'
  
-export default function icon() {
+// Image generation
+export default function Icon() {
   return new ImageResponse(
     (
+      // ImageResponse JSX element
       <div
         style={{
-          fontSize: 28,
-          background: '#6449ff',
+          fontSize: 24,
+          background: '#ffdb4d',
           width: '100%',
           height: '100%',
           display: 'flex',
@@ -25,6 +27,11 @@ export default function icon() {
         O
       </div>
     ),
-    size,
-  );
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported icons size metadata
+      // config to also set the ImageResponse's width and height.
+      ...size,
+    }
+  )
 }
